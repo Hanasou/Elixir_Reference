c4d19540112fe553ef91be29eff768cc66462514
Hanasou <royzhang42@gmail.com>
2021-09-10 01:59:33 -0700

    stuff

--- lib/cards.ex
+++ lib/cards.ex
@@ -15,4 +15,33 @@ defmodule Cards do
   def hello do
     :world
   end
+
+  def create_deck do
+    # Defining a list is pretty similar to Python
+    # Also remember that the last element is what is automatically returned
+    values = ["Ace", "One", "Two"]
+    suits = ["Clubs", "Diamonds", "Hearts", "Spades"]
+    # This for loop means that for each suit inside suits
+    # Do this thing. That part is expected
+    # We can include both enumerables in the same comprehension
+    for suit <- suits, value <- values do
+      # But remember that this for loop is essentially a mapping function
+      # This means that we're creating a brand new list.
+      # In this case, we're creating the exact same list, because we're not doing anything with suit
+      "#{value} of #{suit}" # <- Format print.
+    end
+  end
+
+  def shuffle(deck) do
+    # Don't have to import enum which is pretty cool
+    Enum.shuffle(deck)
+  end
+
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
+  end
+
+  def deal(deck, hand_size) do
+    Enum.split(deck, hand_size)
+  end
 end
