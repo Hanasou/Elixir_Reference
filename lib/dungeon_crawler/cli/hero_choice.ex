bd4d45ffdb7174b5ccd29871ed3f674826a6ff03
Hanasou <royzhang42@gmail.com>
2024-06-01 01:51:46 -0700

    Stuff

--- lib/dungeon_crawler/cli/hero_choice.ex
+++ lib/dungeon_crawler/cli/hero_choice.ex
@@ -1,3 +1,25 @@
 defmodule DungeonCrawler.CLI.HeroChoice do
+  alias Mix.Shell.IO, as: Shell
+
+  def start do
+    Shell.cmd("clear")
+    Shell.info("Start by choosing your hero")
+
+    heroes = DungeonCrawler.Heroes.all()
+
+    heroes
+    |> Enum.map(&(&1.name))
+    |> display_options
+  end
+
+  def display_options(options) do
+    options
+    |> Enum.with_index(1) # This function will pack options in a tuple alongside an index
+    |> Enum.each(fn {option, index} ->
+      Shell.info("#{index} - #{option}") # This is the string interpolation syntax
+    end)
+
+    options
+  end
 
 end
--- lib/dungeon_crawler/cli/main.ex
+++ lib/dungeon_crawler/cli/main.ex
@@ -4,11 +4,17 @@ defmodule DungeonCrawler.CLI.Main do
 
   def start_game() do
     welcome_message()
+    Shell.prompt("Press Enter to continue")
+    hero_choice()
   end
 
   defp welcome_message do
     Shell.info("== Dungeon Crawl ==")
     Shell.info("You awake in a dungeon full of monsters")
     Shell.info("You need to survive and find the exit")
+  end
+
+  defp hero_choice do
+    DungeonCrawler.CLI.HeroChoice.start()
   end
 end
