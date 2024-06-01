40b463d446b3f7047961709f60fe2cabc8606e3c
zhanro01 <roy.zhang@lexisnexisrisk.com>
2024-05-31 17:23:26 -0700

    Character descriptions

--- lib/dungeon_crawler/character.ex
+++ lib/dungeon_crawler/character.ex
@@ -2,7 +2,7 @@ defmodule DungeonCrawler.Character do
   # This is how you define a struct
   defstruct name: nil,
             description: nil,
-            hitpoints: 0,
+            hit_points: 0,
             max_hit_points: 0,
             attack_description: nil,
             damage_range: nil
--- lib/dungeon_crawler/heroes.ex
+++ lib/dungeon_crawler/heroes.ex
@@ -4,8 +4,29 @@ defmodule DungeonCrawler.Heroes do
   alias DungeonCrawler.Character
 
   def all, do: [
-    %Character{},
-    %Character{},
-    %Character{},
+    %Character{
+      name: "Knight",
+      description: "Strong defense and consistent damage",
+      hit_points: 18,
+      max_hit_points: 18,
+      damage_range: 4..5,
+      attack_description: "sword"
+    },
+    %Character{
+      name: "Wizard",
+      description: "Strong attack, low health",
+      hit_points: 8,
+      max_hit_points: 8,
+      damage_range: 6..10,
+      attack_description: "fireball"
+    },
+    %Character{
+      name: "Rogue",
+      description: "High variance attack",
+      hit_points: 12,
+      max_hit_points: 12,
+      damage_range: 1..12,
+      attack_description: "dagger"
+    },
   ]
 end
